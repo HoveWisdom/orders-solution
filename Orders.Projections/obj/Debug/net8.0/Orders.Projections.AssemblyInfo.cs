c6170150a7ef1618659e0d7f8f691bebaeff6a7f
HoveWisdom <hovewisdom@gmail.com>
2025-10-21 11:10:12 +0200

    Step 1: Solution projects creation and installation of Azure SDK packages and dev tools.

--- Orders.Projections/obj/Debug/net8.0/Orders.Projections.AssemblyInfo.cs
+++ Orders.Projections/obj/Debug/net8.0/Orders.Projections.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Orders.Projections")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ef18d40ee3b61d594a311e2ae0a2a372efb4151e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Orders.Projections")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Orders.Projections")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
